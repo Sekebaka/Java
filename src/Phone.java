204a12badd27464a1881b8e069772a07300ef63a
Sebastian Ram <sebo324@o2.pl>
2022-02-15 16:25:53 +0100

    zad 13

--- src/Phone.java
+++ src/Phone.java
@@ -1,29 +1,26 @@
 package com.company.devices;
 import com.company.Human;
 import com.company.Salleable;
-import java.net.URL;
-import java.util.List;
-import java.net.MalformedURLException;
+import java.util.*;
 
 public class Phone extends Device implements Salleable {
     final String producer;
     final String model;
     final Double screenSize;
-    final String operatingSystem;
-    static final String DEFAULT_SERVER = "localhost:3000";
-    static final String DEFAULT_PROTOCOL = "HTTP";
-    static final String DEFAULT_VERSION = "1.0.0";
+    String operatingSystem;
+    HashSet<Application> applications;
 
-    public Phone(String producer, String model, Double screenSize, String operatingSystem,int yearOfProduction,Double value) {
-        super(producer, model, yearOfProduction,value);
+    public Phone(String producer, String model, Double screenSize, String operatingSystem, int yearOfProduction, Double value) {
+        super(producer, model, yearOfProduction, value);
         this.producer = producer;
         this.model = model;
         this.screenSize = screenSize;
         this.operatingSystem = operatingSystem;
+        this.applications = new HashSet<Application>();
     }
 
     public String toString() {
-        return producer+" "+model+" "+screenSize+" "+operatingSystem + " " + yearOfProduction;
+        return producer + " " + model + " " + screenSize + " " + operatingSystem + " " + yearOfProduction;
     }
 
     @Override
@@ -32,60 +29,65 @@ public class Phone extends Device implements Salleable {
     }
 
     @Override
-    public void sell(Human seller, Human buyer, Double price){
-        if (seller.mobilePhone != this){
+    public void sell(Human seller, Human buyer, Double price) {
+        if (seller.mobilePhone != this) {
             System.out.println("Nie możesz sprzedać nie swojego telefonu");
             return;
         }
-        if (buyer.cash < price){
+        if (buyer.cash < price) {
             System.out.println("Kupujący nie ma wystarczająco pieniędzy");
             return;
         }
-        if (seller == buyer){
+        if (seller == buyer) {
             System.out.println("Nie możesz sprzedawać samemu sobie");
             return;
         }
-        buyer.cash -=price;
+        buyer.cash -= price;
         seller.cash += price;
         buyer.mobilePhone = seller.mobilePhone;
         seller.mobilePhone = null;
-        System.out.println("Udało się sprzedać telefon za "+price);
+        System.out.println("Udało się sprzedać telefon za " + price);
 
 
     }
 
 
-    public void installAnApp(String name) {
-        this.installAnApp(name, DEFAULT_VERSION);
-    }
-
-    public void installAnApp(String name, String version) {
-        this.installAnApp(name, version, DEFAULT_SERVER);
-    }
-
-    public void installAnApp(String name, String version, String serverAddress) {
-        try {
-            URL url = new URL(DEFAULT_PROTOCOL, serverAddress, name + " " + version);
-            this.installAnApp(url);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+    public void installAnApp(Human buyer, Application application) throws Exception {
+        if (buyer.cash < application.price) {
+            throw new Exception("Nie masz wystarczajaco pieniedzy");
+        } else {
+            buyer.cash -= application.price;
+            applications.add(application);
         }
     }
 
-    public void installAnApp(URL url) {
-        System.out.println("Pobieranie aplikacji " + url.getFile());
-    }
-    public void installAnApp(List<String> appNames) {
-        String[] appArray = new String[appNames.size()];
-        appArray = appNames.toArray(appArray);
-
-        this.installAnApp(appArray);
+    public boolean isAppInstalled(Application application) {
+        return isAppInstalled(application.name);
     }
 
-    public void installAnApp(String[] applications) {
-        for (String appName : applications) {
-            this.installAnApp(appName);
+    public boolean isAppInstalled(String applicationName) {
+        for (Application app : applications) {
+            if (app.name.equals(applicationName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void allFreeApps() {
+        for (Application application : applications) {
+            if (application.price == 0) {
+                System.out.println(application.name);
+            }
         }
     }
 
+    public double valueOfApps() {
+        Double value = 0.0;
+        for (Application application : applications) {
+            value += application.price;
+        }
+        return value;
+    }
+}
 
